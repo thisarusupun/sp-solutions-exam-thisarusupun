7965d4991daa58b584cd2489d6f9998f536ee0f7
thisarusupun <thisarusupun1314@gmail.com>
2025-07-07 10:12:56 +0530

    problem 1 while loop solution added

--- src/problem1/ProblemOneSolution.java
+++ src/problem1/ProblemOneSolution.java
@@ -1,17 +1,33 @@
 package problem1;
 
-public class ForLoopSolution {
+public class ProblemOneSolution {
 
     public static void main(String[] args) {
         int[] numList = {1,2,3,4,5,6,7,8,9};
-        int sumOfNumbers = findSumUsingForLoop(numList);
-        System.out.println("sum of the numbers " + sumOfNumbers);
+
+        int sumOfNumbersUsingForLoop = findSumUsingForLoop(numList);
+        System.out.println("sum of the numbers using for loop " + sumOfNumbersUsingForLoop);
+
+        int sumOfNumbersUsingWhileLoop = findSumUsingWhileLoop(numList);
+        System.out.println("sum of the numbers using while loop " + sumOfNumbersUsingWhileLoop);
+
+
     }
 
     private static int findSumUsingForLoop(int[] numList) {
         int sum = 0;
         for (int i=0; i< numList.length ; i++){
             sum += numList[i];
+        }
+        return sum;
+    }
+
+    private static int findSumUsingWhileLoop(int[] numList) {
+        int sum = 0;
+        int i = 0;
+        while (i < numList.length){
+            sum += numList[i];
+            i++;
         }
         return sum;
     }
